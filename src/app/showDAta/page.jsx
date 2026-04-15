1aa042ff5271f86e2060caebaed7e28536a20827
fuadhasan199 <fuadhasan740266@gmail.com>
2026-04-15 20:10:46 +0600

    feat: implement NextAuth basic session handling with server and client support

--- src/app/showDAta/page.jsx
+++ src/app/showDAta/page.jsx
@@ -1,13 +1,17 @@
-"use client"
 
-import { useSession } from "next-auth/react";
 
-const ShowData = () => { 
-    const session=useSession()  
-    console.log(session)
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+
+// import { useSession } from "next-auth/react";
+
+const ShowData = async() => { 
+    // const session=useSession()  
+    // console.log(session) 
+    const session=await getServerSession(authOptions)
     return (
         <div>
-             <h2 className="font-bold">Use Client : </h2> 
+             <h2 className="font-bold">Use Server : </h2> 
              <p className="border-2 rounded-2xl p-3"> {JSON.stringify(session)}</p>
         </div>
     );
